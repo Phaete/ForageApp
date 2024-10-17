625ca0f5ff377faa68c839454ede01540213d9b3
-St4n <pel4nk@gmail.com>
2024-10-17 06:48:37 +0200

    Add new model ForageWikiItem, it's corresponding DTO and Enums used by those models

---
+++ backend/src/main/java/com/phaete/backend/forage/model/ForageCategory.java
@@ -0,0 +1,13 @@
+package com.phaete.backend.forage.model;
+
+public enum ForageCategory {
+	FRUIT,
+	NUT,
+	MUSHROOM,
+	HERB,
+	FLOWER,
+	VEGETABLE,
+	WILD_GREEN,
+	ROOTS_TUBERS,
+	OTHER
+}
---
+++ backend/src/main/java/com/phaete/backend/forage/model/ForageWikiItem.java
@@ -0,0 +1,27 @@
+package com.phaete.backend.forage.model;
+
+/**
+ * Representation of a forage wiki item in the database.
+ * A ForageWikiItem is an object representing the static attributes of a forage item.
+ *
+ * @param id          the id of the forage item
+ * @param name        the name of the forage item
+ * @param category    the category of the forage item
+ * @param source      the source of the forage item
+ * @param description the description of the forage item
+ * @param season      the harvestable season of the forage item
+ * @param imageURLs   the image URLs of the forage item
+ *
+ * @author -St4n aka Phaete
+ */
+public record ForageWikiItem(
+		String id,
+		String name,
+		ForageCategory category,
+		ForageSource source,
+		String description,
+		String season,
+		java.util.List<String> imageURLs
+) {
+
+}
