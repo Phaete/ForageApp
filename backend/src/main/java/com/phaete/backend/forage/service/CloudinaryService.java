5023486526eb7ac854a9d11f6525b803587a049a
-St4n <pel4nk@gmail.com>
2024-10-17 10:43:49 +0200

    Add cloudinary dependency to project Add CloudinaryConfig to create Cloudinary instance with API key from environment variable Add CloudinaryService to upload image to Cloudinary Add JUnit Test for CloudinaryService

---
+++ backend/src/main/java/com/phaete/backend/forage/service/CloudinaryService.java
@@ -0,0 +1,36 @@
+package com.phaete.backend.forage.service;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Service class for uploading images to Cloudinary.
+ */
+@Service
+public class CloudinaryService {
+
+	private final Cloudinary cloudinary;
+
+	public CloudinaryService(Cloudinary cloudinary) {
+		this.cloudinary = cloudinary;
+	}
+
+	/**
+	 * Uploads the given image to Cloudinary and returns its URL.
+	 *
+	 * @param image The image to upload.
+	 * @return The URL of the uploaded image.
+	 * @throws IOException If the upload fails.
+	 */
+	public String uploadImage(MultipartFile image) throws IOException {
+		File fileToUpload = File.createTempFile("file", null);
+		image.transferTo(fileToUpload);
+		Map response = cloudinary.uploader().upload(fileToUpload, Map.of());
+		return response.get("url").toString();
+	}
+}
