f89c09bdea61d25c9f28df03251708d744d833a2
-St4n <pel4nk@gmail.com>
2024-10-18 14:11:14 +0200

    Fix security hotspot in MapComponent

--- frontend/src/forage/components/mapComponent/MapComponent.tsx
+++ frontend/src/forage/components/mapComponent/MapComponent.tsx
@@ -4,12 +4,10 @@ import {MapContainer, TileLayer} from 'react-leaflet'
 import L from "leaflet";
 import * as React from "react";
 import {useState} from "react";
-import axios from "axios";
 
 export default function MapComponent()  {
 
-	const [mapCenter, setMapCenter] = useState<{ position: [number, number], zoom: number }>(
-		{ position: [51, 10], zoom: 6})
+	const [mapCenter, setMapCenter] = useState<{ position: [number, number], zoom: number }>({position: [0, 0], zoom: 1})
 
 	React.useEffect(() => {
 		L.Icon.Default.mergeOptions({
@@ -17,40 +15,8 @@ export default function MapComponent()  {
 			iconUrl: "leaflet/dist/images/marker-icon.png",
 			shadowUrl: "leaflet/dist/images/marker-shadow.png"
 		});
-
-		navigator.geolocation.getCurrentPosition(
-			(position) => {
-				L.map("map").setView([position.coords.latitude, position.coords.longitude], 13);
-			setMapCenter({position: [position.coords.latitude, position.coords.longitude], zoom: 13});
-			},
-			() => {
-				const city = prompt("Please enter your city or town: ");
-				if (city) {
-					getCityPosition(city)
-				} else {
-					setMapCenter({ position: [51, 10], zoom: 6});
-				}
-		})
-	}, []);
-
-
-	async function getCityPosition(city: string) {
-		try {
-			axios.create({
-				headers: {
-					"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0",
-					"Referer": "https://nominatim.openstreetmap.org/ui/search.html"
-				}
-			})
-			const response = await axios.get(`https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(city)}&polygon_geojson=1&format=jsonv2`);
-			const data = response.data[0];
-			const location = data.latlon;
-			console.log([location.lat, location.lng])
-			setMapCenter({position: [location.lat, location.lng], zoom: 13});
-		} catch (error) {
-			console.error("Error getting city position:", error);
-		}
-	}
+		setMapCenter({position: [51, 10], zoom: 6})
+	}, [])
 
 	return (
 		<MapContainer className={"map-size-100"} center={mapCenter.position} zoom={mapCenter.zoom}>
