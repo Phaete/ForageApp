32565070dc4cbd45a9156c49a0f97e01919cd286
-St4n <pel4nk@gmail.com>
2024-11-04 08:32:10 +0100

    Further improvement to forageMapItem popup

--- frontend/src/forage/components/content/forageMapItemView/ForageMapItemView.tsx
+++ frontend/src/forage/components/content/forageMapItemView/ForageMapItemView.tsx
@@ -12,11 +12,17 @@ export default function ForageMapItemView(props: Readonly<ForageMapItemViewProps
 	return (
 		<div className={"w-50"}>
 			<h3>{props.forageMapItem.forageWikiItem.name}</h3>
-            <p>Position: [{props.forageMapItem.position.latitude.toFixed(4)}, {props.forageMapItem.position.longitude.toFixed(4)}]</p>
-            <p>Category: {props.forageMapItem.forageWikiItem.category}</p>
-            <p>Source: {props.forageMapItem.forageWikiItem.source}</p>
-            <p>Season: {props.forageMapItem.forageWikiItem.season}</p>
-            <p>Description: {props.forageMapItem.forageWikiItem.description}</p>
+            <p>
+                Position: [{props.forageMapItem.position.latitude.toFixed(4)}, {props.forageMapItem.position.longitude.toFixed(4)}]<br/>
+                Quality: {props.forageMapItem.assessment.quality} <br/>
+                Quantity: {props.forageMapItem.assessment.quantity}
+            </p>
+            <p>
+                Category: {props.forageMapItem.forageWikiItem.category}<br/>
+                Source: {props.forageMapItem.forageWikiItem.source}<br/>
+                Season: {props.forageMapItem.forageWikiItem.season}<br/>
+                Description: {props.forageMapItem.forageWikiItem.description}
+            </p>
             <div className={"flex flex-row-reverse"}>
                 <button type={"button"} onClick={() => handleDelete()}>
                     Delete
